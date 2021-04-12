5e8f17dacda5be28400114feea7316a13c092cc0
Andrew <andrewsabatyuk17@gmail.com>
2021-04-12 22:33:54 +0300

    create main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import Navbar from './components/Navbar'
+import { GlobalStyle } from './components/globalStyles';
+import Main from './components/Main';
 
 function App() {
   return (
     <Router>
-     <Navbar />
+     <GlobalStyle/>
+     <Main/>
     </Router>
   );
 }
